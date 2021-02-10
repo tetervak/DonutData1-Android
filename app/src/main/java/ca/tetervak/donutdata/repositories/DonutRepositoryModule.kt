307a857d28f6a436dbb0889b4e79c3a947349679
Alex Tetervak <tetervak@sheridancollege.ca>
2021-02-10 08:21:15 -0500

    added the DonutRepositoryModule

---
+++ app/src/main/java/ca/tetervak/donutdata/repositories/DonutRepositoryModule.kt
@@ -0,0 +1,13 @@
+package ca.tetervak.donutdata.repositories
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DonutRepositoryModule {
+    @Binds
+    abstract fun bindEnvelopeRepository(repository: DonutRepositoryRoom): DonutRepository
+}
