3d1f5915d476cb58cb79a19c389120fbf5db0694
Alex Tetervak <tetervak@sheridancollege.ca>
2021-02-10 11:06:56 -0500

    added the donut count to the list

---
+++ app/src/main/java/ca/tetervak/donutdata/binding/DonutCountBindingAdapter.kt
@@ -0,0 +1,13 @@
+package ca.tetervak.donutdata.binding
+
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import ca.tetervak.donutdata.R
+import ca.tetervak.donutdata.domain.Donut
+
+@BindingAdapter("donutCount")
+fun bindDonutCount(textView: TextView, list: List<Donut>?){
+    val count = list?.size ?: 0;
+    textView.text =
+        textView.resources.getQuantityString(R.plurals.donut_count, count, count)
+}
