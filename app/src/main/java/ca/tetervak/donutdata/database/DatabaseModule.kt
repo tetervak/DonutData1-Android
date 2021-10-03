3f9873cc8d8056680d7c1b309133b9babf053c3c
Alexander Tetervak <tetervak@sheridancollege.ca>
2021-10-02 23:26:17 -0400

    simplified the database class logic

--- app/src/main/java/ca/tetervak/donutdata/database/DatabaseModule.kt
+++ app/src/main/java/ca/tetervak/donutdata/database/DatabaseModule.kt
@@ -1,11 +1,14 @@
 package ca.tetervak.donutdata.database
 
-import android.app.Application
+import android.content.Context
 import android.util.Log
+import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -14,8 +17,19 @@ object DatabaseModule {
     private const val TAG = "DataBaseModule"
 
     @Provides
-    fun provideDonutDao(application: Application): DonutDao {
+    fun provideDonutDao(database: DonutDatabase): DonutDao {
         Log.d(TAG, "provideDonutDao: the DonutDao object is returned")
-        return DonutDatabase.getInstance(application).donutDao()
+        return database.donutDao()
+    }
+
+    @Singleton
+    @Provides
+    fun provideDonutDatabase(@ApplicationContext context: Context): DonutDatabase {
+        Log.d(TAG, "provideDonutDatabase: the database object is created")
+        return Room.databaseBuilder(
+            context,
+            DonutDatabase::class.java,
+            "donut_database"
+        ).build()
     }
 }
--- app/src/main/java/ca/tetervak/donutdata/database/DonutDatabase.kt
+++ app/src/main/java/ca/tetervak/donutdata/database/DonutDatabase.kt
@@ -1,38 +1,10 @@
 package ca.tetervak.donutdata.database
 
-import android.content.Context
-import android.util.Log
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [DonutEntity::class], version = 1, exportSchema = false)
 abstract class DonutDatabase : RoomDatabase() {
 
     abstract fun donutDao(): DonutDao
-
-    companion object {
-
-        private const val TAG = "DonutDatabase"
-
-        @Volatile private var INSTANCE: DonutDatabase? = null
-
-        fun getInstance(context: Context): DonutDatabase {
-            val tempInstance = INSTANCE
-            if (tempInstance != null) {
-                Log.d(TAG, "getInstance: the database object is returned")
-                return tempInstance
-            }
-            synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context,
-                    DonutDatabase::class.java,
-                    "donut_database"
-                ).build()
-                INSTANCE = instance
-                Log.d(TAG, "getInstance: the database object is created")
-                return instance
-            }
-        }
-    }
 }
